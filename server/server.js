7797eab1231550adc3a3fb75e41dbd71d2a6eef6
Marcos Nicolau <marcosnicolau2003@gmail.com>
2021-03-08 21:00:12 -0300

    Made enviroment changes

--- server/server.js
+++ server/server.js
@@ -13,9 +13,9 @@ const productsRoutes = require('./routes/products-routes');
 const paymentRoutes = require('./routes/payment-routes');
 require('dotenv').config();
 
-const PORT = process.env.PORT || '3001';
+const PORT = process.env.PORT || '8080';
 const app = express();
-const db = process.env.DB;
+const db = process.env.MONGODB_URI || process.env.DB;
 
 app.use(cors());
 app.use(bodyParser.json({ limit: '50mb' }));
@@ -35,7 +35,7 @@ mongoose.connect(db, { useUnifiedTopology: true, useNewUrlParser: true })
 .catch((err) => console.log(err));
 
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('./build'));
+    app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
     });
